8b847569aa123565354bfd1bfbb6274228c750b0
svitlarp <svitlanka.ripa@gmail.com>
2025-07-21 16:48:25 +0300

    improve Alert Styles

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,27 +1,47 @@
 import '../css/Alert.css'
 
 
-const getBackgroundColor = (variant) => {
-    switch (variant) {
-        case "info":
-            return "bleu";
-        case "error":
-            return "red";
-        case "success":
-            return "green";
-        case "warning":
-            return "orange";
-        default:
-            throw new Error(`Unsupported variant prop value - ${variant}`
-        );
+// const getBackgroundColor = (variant) => {
+//     switch (variant) {
+//         case "info":
+//             return "bleu";
+//         case "error":
+//             return "red";
+//         case "success":
+//             return "green";
+//         case "warning":
+//             return "orange";
+//         default:
+//             throw new Error(`Unsupported variant prop value - ${variant}`
+//         );
+//     }
+// }
+
+
+export const Alert = ({ variant, outlined, elevated, children }) => {
+    // const classNames = ["alert", `alert-${variant}`];
+     const classNames = ["alert", variant];
+
+    if (outlined) {
+        classNames.push("is-outlined");
     }
-}
 
+    if (elevated) {
+        classNames.push("is-elevated");
+    }
 
-export const Alert = ({ variant, children }) => {
     return (
-        // <p className="alertStyles">{children}</p>
-        <p className={`alertStyles alert-${variant}`}> {children}</p >
+        // <p className="alertStyles">{children}</p>   1
+        // <p className={`alertStyles alert-${variant}`}> {children}</p >   2
+        <p className={classNames.join(" ")}> {children}</p >   
+
     );
 }
 
+
+
+// const Alert = ({ variant, children }) => {
+//   const classNames = ["alert", variant];
+
+//   return <p className={classNames.join(" ")}>{children}</p>;
+// };
