2f204826204b27134143a98f6792bb9dd3c0dd24
svitlarp <svitlanka.ripa@gmail.com>
2025-07-22 18:06:27 +0300

    install libs: clsx, modern-normalize, react-icons

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,29 +1,45 @@
-import '../css/Alert.css';
 import clsx from "clsx";
+import '../css/Alert.css';
+import { DiCodeBadge } from "react-icons/di";
 
 
 export const Alert = ({ variant, outlined, elevated, children }) => {
-    // const classNames = ["alert", `alert-${variant}`];
-     const classNames = ["alert", variant];
-
-    if (outlined) {
-        classNames.push("is-outlined");
-    }
-
-    if (elevated) {
-        classNames.push("is-elevated");
-    }
-
     return (
-        // <p className="alertStyles">{children}</p>   1
-        // <p className={`alertStyles alert-${variant}`}> {children}</p >   2
-        <p className={classNames.join(" ")}> {children}</p >   
-
-    );
+        <>
+        <DiCodeBadge />
+        <p className={clsx(
+            "alert",
+            variant,
+            outlined && "is-outlined",
+            elevated && "is-elevated"
+            )}>{children}</p>
+        </>
+    )
 }
 
 
+// ________________________________________________________
+// export const Alert = ({ variant, outlined, elevated, children }) => {
+//      const classNames = ["alert", variant];
 
+//     if (outlined) {
+//         classNames.push("is-outlined");
+//     }
+
+//     if (elevated) {
+//         classNames.push("is-elevated");
+//     }
+
+//     return (
+//         // <p className="alertStyles">{children}</p>   1
+//         // <p className={`alertStyles alert-${variant}`}> {children}</p >   2
+//         <p className={classNames.join(" ")}> {children}</p >   
+
+//     );
+// }
+
+
+// ________________________________________________________
 // const Alert = ({ variant, children }) => {
 //   const classNames = ["alert", variant];
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize";
+// import './css/index.css';
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
